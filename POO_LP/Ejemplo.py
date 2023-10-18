ca4c57c9f05eea39a41e8aef4089d83fa788c1d2
mario quispe <manuelmarioquispe27@gmail.com>
2023-10-18 12:58:38 -0500

    clases de lambda

--- POO_LP/Ejemplo.py
+++ POO_LP/Ejemplo.py
@@ -56,16 +56,29 @@ class Producto:
         producto_eliminar=producto[id-1]
         return f"El siguiente producto fue eliminado {producto_eliminar}" 
 
-    def actualizar_producto (self,id):
-        pass
+    # def actualizar_producto (self,id,clave,valor):
+    #     producto_actualizar=list(filter(lambda obj: obj[clave]==id, producto))
+
+    def actualizar_producto (self,id,clave,valor):
+        ol=valor
+        producto_actualizar=list(filter(lambda obj:obj[clave]==id,productos))[0].update
+        ({clave:valor})
+        return "se actualizo"
+
+#list funcion para crear lista 
+# programacion orientada a python
+# metodo funcional filter retorna una lista con el elemento 
+# 
+# 
 
 prod=Producto("aceite","extra virgen",1,"botella x litro",12.50)
-print(prod.registrar_producto(2))
+print(prod.registrar_producto())
 print(prod.mostrar_producto())
 print(prod.mostrar_producto(1))
 print(prod.eliminar_producto(2))
 print(prod.mostrar_producto())
-
+# print(prod.actualizar_producto("2023-A","codigo","cualquier cosa"))
+print(prod.actualizar_producto(125,"precio",130))
 # casos de uso de estudiante
 
 
